2b29540361e22ffc853d7879f788f41a2f781460
Dualify <niitenriquefederico@gmail.com>
2026-01-13 14:00:19 +0200

    rss uudised

--- xmlKuvamine.php
+++ xmlKuvamine.php
@@ -1,5 +1,6 @@
 <?php
-$opilased=simplexml_load_file("opilased.xml");
+$opilased=simplexml_load_file("opilase.xml");
+$feed=simplexml_load_file("https://www.err.ee/rss");
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,6 +12,45 @@ $opilased=simplexml_load_file("opilased.xml");
 <?php
 //1. Opilase nimi
 echo "1.Opilase nimi: ".$opilased->opilane[0]->nimi;
+?>
+<table>
+    <tr>
+        <th>
+            Opilase nimi
+        </th>
+        <th>
+            Isikukood
+        </th>
+        <th>
+            Eriala
+        </th>
+        <th>
+            Elukoht
+        </th>
+    </tr>
+    <?php
+    foreach($opilased->opilane as $opilane){
+        echo "<tr>";
+        echo "<td>".$opilane->nimi."</td>";
+        echo "<td>".$opilane->isikukood."</td>";
+        echo "<td>".$opilane->eriala."</td>";
+        echo "<td>".$opilane->elukoht->linn." , ".$opilane->elukoht->maakond."</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+<h1>RSS uudiste lugemine</h1>
+<?php
+echo "<ul>";
+foreach($feed->channel->item as $item){
+    echo "<li>";
+    echo "<a href='$item->link' target='_blank'>".$item->title."</a>";
+    echo $item->description;
+    echo " Kuupäev ".$item->pubDate;
+    echo "</li>";
+
+}
+echo "</ul>";
 ?>
 </body>
 </html>
